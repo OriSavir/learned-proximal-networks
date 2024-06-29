aea2f44b8d18f01b042f86b61e1335c2fc25f9c1
OriSavir <orisavir@gmail.com>
2024-06-29 18:28:28 -0400

    Make ne mnist cnn

---
+++ lpn/networks/ne_mnist.py
@@ -0,0 +1,168 @@
+"""Normalization Equivariant Learned proximal networks for input size of 28x28 (e.g. MNIST)."""
+
+import torch
+from torch import nn
+from ..utils.norm_equiv import AffineConv2d
+from ..utils.norm_equiv import SortPool
+
+
+
+
+def get_padding(kernel_size, dilation):
+    return (kernel_size + (kernel_size - 1) * (dilation - 1) - 1) // 2
+
+
+class LPN(nn.Module):
+    def __init__(
+        self,
+        in_dim,
+        hidden,
+        layers=1,
+        beta=1,
+        kernel_size=3,
+        stride=1,
+        dilation=1,
+        alpha=0.0,
+    ):
+        super(LPN, self).__init__()
+
+
+        # conv kernel size
+        '''
+        if type(kernel_size) == int:
+            kernel_size = [kernel_size] * layers
+        # conv dilation
+        if type(dilation) == int:
+            dilation = [dilation] * layers
+        # conv stride
+        if type(stride) == int:
+            stride = [stride] * layers
+        '''
+
+        self.hidden = hidden
+        self.lin = nn.ModuleList(
+            [
+                AffineConv2d(
+                    in_channels=in_dim,
+                    out_channels=hidden,
+                    kernel_size=kernel_size,
+                    stride=stride,
+                    dilation=dilation,
+                    blind=True,
+                    padding=1
+                ),
+                AffineConv2d(
+                    in_channels=hidden,
+                    out_channels=hidden,
+                    kernel_size=kernel_size,
+                    stride=2,
+                    dilation=dilation,
+                    blind=True,
+                    padding=1
+                ),
+                AffineConv2d(
+                    in_channels=hidden,
+                    out_channels=hidden,
+                    kernel_size=kernel_size,
+                    stride=stride,
+                    dilation=dilation,
+                    blind=True,
+                    padding=1
+                ),
+                AffineConv2d(
+                    in_channels=hidden,
+                    out_channels=hidden,
+                    kernel_size=kernel_size,
+                    stride=2,
+                    dilation=dilation,
+                    blind=True,
+                    padding=1
+                ),
+                nn.Linear(hidden * 7 * 7, 64, bias=False),
+                nn.Linear(64, 1, bias=False),
+            ]
+        )
+
+        self.res = nn.ModuleList(
+            [
+                AffineConv2d(
+                    in_channels=in_dim,
+                    out_channels=hidden,
+                    kernel_size=kernel_size,
+                    stride=2,
+                    dilation=dilation,
+                    blind=True,
+                    padding=1
+                ),
+                AffineConv2d(
+                    in_channels=in_dim,
+                    out_channels=hidden,
+                    kernel_size=kernel_size,
+                    stride=stride,
+                    dilation=dilation,
+                    blind=True,
+                    padding=1
+                ),
+                AffineConv2d(
+                    in_channels=in_dim,
+                    out_channels=hidden,
+                    kernel_size=kernel_size,
+                    stride=2,
+                    dilation=dilation,
+                    blind=True,
+                    padding=1
+                ),
+                nn.Linear(7 * 7 * in_dim, 64, bias=False),
+            ]
+        )
+
+        self.act = SortPool()
+        self.alpha = alpha
+
+    def scalar(self, x):
+        y = x.clone()
+        y = self.act(self.lin[0](y))
+        size = [28, 14, 14, 7]
+        for core, res, sz in zip(self.lin[1:-2], self.res[:-1], size[:-1]):
+            x_scaled = nn.functional.interpolate(x, (sz, sz), mode="bilinear")
+            #print(core(y).shape)
+            #print(res(x_scaled).shape)
+            y = self.act(core(y) + res(x_scaled))
+
+        y = y.reshape(y.shape[0], -1)
+        x_scaled = nn.functional.interpolate(
+            x, (size[-1], size[-1]), mode="bilinear"
+        ).reshape(x.shape[0], -1)
+        y = self.lin[-2](y) + self.res[-1](x_scaled)
+        y = self.act(y)
+
+        y = self.lin[-1](y)
+        # return shape: (batch, 1)
+
+        y = y**2 + self.alpha * x.pow(2).sum(dim=(1, 2, 3)).unsqueeze(1)
+
+        return y
+
+    def init_weights(self, mean, std):
+        print("init weights")
+        with torch.no_grad():
+            for core in self.lin[1:]:
+                core.weight.data.normal_(mean, std).exp_()
+
+    # this clips the weights to be non-negative to preserve convexity
+    def wclip(self):
+        with torch.no_grad():
+            for core in self.lin[1:]:
+                core.weight.data.clamp_(0)
+
+    def forward(self, x):
+        with torch.enable_grad():
+            if not x.requires_grad:
+                x.requires_grad = True
+            x_ = x
+            y = self.scalar(x_)
+            grad = torch.autograd.grad(
+                y.sum(), x_, retain_graph=True, create_graph=True
+            )[0]
+
+        return grad
