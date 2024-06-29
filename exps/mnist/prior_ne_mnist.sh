380b57940642ea1f322d89a1438f7b41b9e03848
OriSavir <orisavir@gmail.com>
2024-06-29 18:33:51 -0400

    train models

---
+++ exps/mnist/prior_ne_mnist.sh
@@ -0,0 +1,10 @@
+
+for perturb in convex blur gaussian; do
+    python lpn/evaluate_prior.py \
+    --model_config_path exps/mnist/configs/mnist/model_ne.json \
+    --out_dir exps/mnist/experiments/ne_mnist/prior \
+    --perturb_config_path exps/mnist/configs/mnist/prior/perturb/${perturb}.json \
+    --dataset_config_path exps/mnist/configs/mnist/prior/dataset.json \
+    --model_path exps/mnist/experiments/ne_mnist/model.pt \
+    --inv_alg cvx_cg 
+done
